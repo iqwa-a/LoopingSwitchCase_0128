e7e2a9a936487bdfc707f258b2a79248ac945102
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-18 19:40:59 +0700

    membuat int main

--- file3.cpp
+++ file3.cpp
@@ -8,3 +8,18 @@ float luasPersegiPanjang(float x, float y){
 float luaslingkaran (float x){
     return 3.14 * x * x;
 }
+
+int main () {
+    int pilihan;
+    float panjang,lebar,jarijari;
+    do{
+        cout <<"===Men==="<<endl;
+        cout << " 1. Menghitung Luas Persegi Panjang " << endl;
+        cout << " 2. Menghitung Luas Lingkaran " << endl;
+        cout << " 3. Exit " << endl;
+        cout << "Pilih Menu (1/2/3) ";
+        cin >> pilihan;
+
+        
+    }
+}
