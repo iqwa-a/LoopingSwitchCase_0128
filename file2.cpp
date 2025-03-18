53792b0a81af8555bee699da2ed6e8f25cc5068d
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-18 19:11:16 +0700

    membuat int variable

---
+++ file2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std; 
+
+int main() {
+    //perulangan while
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+
+}
+
+
