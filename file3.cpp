5baac5656f593134044be9ec10e6f2c0df41ca94
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-18 19:53:48 +0700

    Membuat Switch case

--- file3.cpp
+++ file3.cpp
@@ -20,6 +20,28 @@ int main () {
         cout << "Pilih Menu (1/2/3) ";
         cin >> pilihan;
 
-        
-    }
+        switch (pilihan){
+        case 1 :
+            cout << "menghitung Luas Persegi Panjang " << endl;
+            cout << "Masukan Panjang "  ;
+            cin >> panjang;
+            cout << "menghitung Lebar "  ;
+            cin >> lebar;
+            cout << "Luas PErsegi Panjang = " 
+            << luasPersegiPanjang(panjang,lebar) << endl;
+            break;
+        case 2 : 
+        cout << "menghitung Luas Lingkaran " << endl;
+        cout << "masukan jari jari = " ;
+        cin >> jarijari;
+        cout << "Luas Lingkaran = "
+        << luaslingkaran(jarijari) << endl;
+        break;
+        case 3 : 
+        break;
+        default :
+        cout << "Pilihan tidak ada" << endl;
+            break;
+        }
+    }while(pilihan != 3);
 }
