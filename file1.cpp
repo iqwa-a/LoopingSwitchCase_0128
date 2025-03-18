8aaea732c248802eac40a2863a4aafb928833b46
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-03-18 19:01:36 +0700

    Membuat Int main

---
+++ file1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    //Pencacah Naik 
+    int i;
+    for (i=0; i < 5; i++){
+        cout << "Nilai i = "<< i << "Lari keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+}
